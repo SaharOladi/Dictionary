7f1a88eea7fed8e9230380c78bee4d62b04bc21d
SaharOladi <sahar.olady@gmail.com>
2020-10-11 08:42:21 +0330

    Initial commit

---
+++ app/src/main/java/com/example/dictionary/model/Word.java
@@ -0,0 +1,73 @@
+package com.example.dictionary.model;
+
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.UUID;
+
+@Entity(tableName = "wordTable")
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private long primaryId;
+
+    @ColumnInfo(name = "uuid")
+    private UUID mId;
+
+    @ColumnInfo(name = "word title")
+    private String mTitle;
+
+    @ColumnInfo(name = "word mean")
+    private String mMean;
+
+
+    public Word(UUID id) {
+        mId = id;
+    }
+
+    public Word(UUID id, String title, String mean) {
+        mId = id;
+        mTitle = title;
+        mMean = mean;
+    }
+
+    public Word(String title, String mean) {
+        mTitle = title;
+        mMean = mean;
+    }
+
+    public long getPrimaryId() {
+        return primaryId;
+    }
+
+    public void setPrimaryId(long primaryId) {
+        this.primaryId = primaryId;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getMean() {
+        return mMean;
+    }
+
+    public void setMean(String mean) {
+        mMean = mean;
+    }
+}
