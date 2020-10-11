8f4167b823d894440810284303df130140193080
SaharOladi <sahar.olady@gmail.com>
2020-10-11 09:32:45 +0330

    Add recyclerView layout and word item row layout.

--- app/src/main/java/com/example/dictionary/controller/activity/MainActivity.java
+++ app/src/main/java/com/example/dictionary/controller/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.dictionary.controller;
+package com.example.dictionary.controller.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
---
+++ app/src/main/java/com/example/dictionary/controller/fragment/WordListFragment.java
@@ -0,0 +1,40 @@
+package com.example.dictionary.controller.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dictionary.R;
+
+
+public class WordListFragment extends Fragment {
+
+
+    public WordListFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static WordListFragment newInstance() {
+        WordListFragment fragment = new WordListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_word_list, container, false);
+    }
+}
